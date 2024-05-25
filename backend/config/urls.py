c94dff43c2171ed2e8993f7a8ed9cfbb78f785d7
ignaciodz <ignaciodiazbr@gmail.com>
2024-05-25 04:59:59 -0500

    add: se agrego las urls para producto y categoria

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from producto.api.router import router_producto, router_categoria
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router_producto.urls)),
+    path('api/', include(router_categoria.urls)),
 ]
